583cb53e87dfa612e351dc23b6d43af2098d1561
Jose Vicente Nunez <josevnz@kodegeek.com>
2024-03-16 19:17:38 -0400

    Skeleton code for even_log application test

--- test/test_alert_apps.py
+++ test/test_alert_apps.py
@@ -1,9 +1,23 @@
 import unittest
+from pathlib import Path
+
+from suricatalog.alert_apps import TableAlertApp
+from suricatalog.filter import BaseFilter, OnlyAlertsFilter
+
+BASEDIR = Path(__file__).parent
+EVE_FILE = BASEDIR.joinpath("eve.json")
 
 
 class AlertAppsTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    async def test_eve_log(self):
+        timestamp_filter: BaseFilter = OnlyAlertsFilter()
+        app = TableAlertApp()
+        app.title = "Dummy title"
+        app.set_filter(timestamp_filter)
+        app.set_eve_files([EVE_FILE])
+        self.assertIsNotNone(app)
+        async with app.run_test() as pilot:
+            pass
 
 
 if __name__ == '__main__':
